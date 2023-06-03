89ee4cd2f95a6fb27e566310d82f4a91b3819868
Frances Cornwall <113309999+francescorn@users.noreply.github.com>
2023-06-02 18:58:45 -0700

    Update 58_Length_of_Last_Word.py

--- 58_Length_of_Last_Word.py
+++ 58_Length_of_Last_Word.py
@@ -1,5 +1,4 @@
 class Solution:
     def lengthOfLastWord(self, s: str) -> int:
         s = s.rstrip()
-        s_list = s.split()
-        return len(s_list[-1])
+        return len(s.split()[-1])
