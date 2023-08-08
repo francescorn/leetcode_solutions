f929ee0317bcd2680369147a828e11625c6a8ae6
Frances Cornwall <113309999+francescorn@users.noreply.github.com>
2023-08-08 11:55:22 -0700

    Update 2621_Sleep.js

--- 2621_Sleep.js
+++ 2621_Sleep.js
@@ -1,3 +1,12 @@
+/**
+ * @param {number} millis
+ */
 async function sleep(millis) {
-     return new Promise(res => setTimeout(res, millis));
+     const p = new Promise(res => setTimeout(res, millis));
+     return p;
 }
+
+/** 
+ * let t = Date.now()
+ * sleep(100).then(() => console.log(Date.now() - t)) // 100
+ */
