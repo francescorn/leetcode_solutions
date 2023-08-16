60d44c2a825aec277d0f4f08e6e92d9267076684
Frances Cornwall <113309999+francescorn@users.noreply.github.com>
2023-08-16 10:19:35 -0700

    Update 7_reverse_integer.py

--- 7_reverse_integer.py
+++ 7_reverse_integer.py
@@ -1,12 +1,18 @@
 class Solution:
     def reverse(self, x: int) -> int:
-        y = abs(x)
-        y = str(y)
-        y = y[::-1]
-        y = int(y)
-        if y > 2 ** 31:
-            return 0      
-        elif x < 0:
-            y = y * -1
-            return y
-        return y
+        if x < 0:
+            x = abs(x)
+            x = str(x)
+            x = x[::-1]
+            x = int(x)
+            if x > 2 ** 31:
+                return 0
+            x = x * -1
+            return x
+        else:
+            x = str(x)
+            x = x[::-1]
+            x = int(x)
+            if x > 2 ** 31:
+                return 0
+            return x
