8062e2f6a457450329cbcba10479b67e122fc58d
Frances Cornwall <113309999+francescorn@users.noreply.github.com>
2023-08-16 10:39:50 -0700

    Create 17_Letter_Combinations_of_a_Phone_Number.py

---
+++ 17_Letter_Combinations_of_a_Phone_Number.py
@@ -0,0 +1,21 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        dictionary = {
+            "2": "abc",
+            "3": "def",
+            "4": "ghi",
+            "5": "jkl",
+            "6": "mno",
+            "7": "pqrs",
+            "8": "tuv",
+            "9": "wxyz"
+        }
+        letters = []
+        for digit in digits:
+            letters.append(dictionary[digit])
+        list_1 =[]
+        for chars in itertools.product(*letters):
+            list_1.append("".join(chars))
+        if not digits:
+            return []
+        return list_1
